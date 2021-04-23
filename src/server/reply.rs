dd6d277bd2958ec2f5881889149a17e9e16e7c60
Matt Butcher <matt.butcher@microsoft.com>
2021-04-23 16:30:18 -0600

    removed old TOML serializer
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- src/server/reply.rs
+++ src/server/reply.rs
@@ -98,42 +98,6 @@ impl Reply for SerializedData {
     }
 }
 
-/*
-// Borrowed and modified from https://docs.rs/warp/0.2.5/src/warp/reply.rs.html#102
-pub fn toml<T>(val: &T) -> Toml
-where
-    T: Serialize,
-{
-    Toml {
-        inner: toml::to_vec(val).map_err(|e| {
-            tracing::log::error!("Error while serializing TOML: {:?}", e);
-        }),
-    }
-}
-
-/// A JSON formatted reply.
-pub struct Toml {
-    inner: Result<Vec<u8>, ()>,
-}
-
-impl Reply for Toml {
-    #[inline]
-    fn into_response(self) -> Response {
-        match self.inner {
-            Ok(body) => {
-                let mut res = Response::new(body.into());
-                res.headers_mut().insert(
-                    warp::http::header::CONTENT_TYPE,
-                    warp::http::header::HeaderValue::from_static(TOML_MIME_TYPE),
-                );
-                res
-            }
-            Err(()) => warp::http::StatusCode::INTERNAL_SERVER_ERROR.into_response(),
-        }
-    }
-}
-*/
-
 /// A helper function for converting a [`ProviderError`](crate::provider::ProviderError) into a Warp
 /// `Reply` with the proper status code. It will return a TOML body that looks like:
 /// ```toml
