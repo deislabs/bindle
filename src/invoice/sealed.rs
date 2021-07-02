f74599b0a30f57816c4e308e1b5161cc807de585
Taylor Thomas <taylor.thomas@microsoft.com>
2021-07-02 11:43:09 -0600

    Adds documentation to sealed trait

--- src/invoice/sealed.rs
+++ src/invoice/sealed.rs
@@ -1,1 +1,3 @@
+/// A Marker trait that should not be exposed publicly. It is used to mark a trait as "sealed",
+/// which means that the trait cannot be implemented outside of this crate
 pub trait Sealed {}
